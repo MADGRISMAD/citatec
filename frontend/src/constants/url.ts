0e4c999987fb0c9f3fc05ae26047647115c3a220
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-11-30 20:43:34 -0600

    dns setup

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://localhost:3000';
+const BACKEND_DNS = process.env.REACT_APP_BACKEND_DNS || "localhost";
+export const BACKEND_URL = "http://" + BACKEND_DNS + ":3000";
