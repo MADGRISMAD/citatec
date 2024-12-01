a29cfa7cccdc250066f7de9e4ea59f7c039045e6
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-11-30 20:44:27 -0600

    replaced env name

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,2 +1,2 @@
-const BACKEND_DNS = process.env.REACT_APP_BACKEND_DNS || "localhost";
+const BACKEND_DNS = process.env.BACKEND_DNS || "localhost";
 export const BACKEND_URL = "http://" + BACKEND_DNS + ":3000";
