ecc5f7fb58340963a800d1c0437faedb8b437ac3
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-12-28 20:58:30 -0800

    a

--- backend/src/telemetry/index.ts
+++ backend/src/telemetry/index.ts
@@ -1,4 +1,3 @@
-/*instrumentation.ts*/
 import * as opentelemetry from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
